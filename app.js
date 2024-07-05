faadafc4a5ca1353573407879597b14d3915be6f
laasov <lassi.o.virtanen@gmail.com>
2024-07-05 12:35:20 +0300

    Add buttons

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
 import { Hono } from "https://deno.land/x/hono@v3.12.11/mod.ts";
 import * as feedback from "./feedbacks.js";
 
+const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
 const app = new Hono();
+const rat = {"Poor": "1", "Fair": "2", "Good": "3", "Very good": "4", "Excellent": "5"};
+
+app.get("/", (c) => c.html(eta.render("index.eta")));
 
 app.get(
   "/feedbacks/:id", 
@@ -11,11 +16,13 @@ app.get(
 });
 
 app.post(
-  "/feedbacks/:id",
+  "/feedbacks",
   async (c) => {
-    const number = c.req.param("id");
-    await feedback.incrementAt(number);
-    return c.text(`Feedback ${number}: ${await feedback.getFb(number)}`);
+    const body = await c.req.parseBody();
+    const val = body.rating;
+    const id = rat[val];
+    await feedback.incrementAt(id);
+    return c.redirect("/");
 });
 
 export default app;
