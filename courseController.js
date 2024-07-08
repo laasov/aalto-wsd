b11bebe4e4ccf4fdfcc33c4a42edd9042abcb295
laasov <lassi.o.virtanen@gmail.com>
2024-07-09 00:37:35 +0300

    Add name validation

--- courseController.js
+++ courseController.js
@@ -1,7 +1,12 @@
 import { Eta } from "https://deno.land/x/eta@v3.4.0/src/index.ts";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
 import * as courseService from "./courseService.js";
 
 const eta = new Eta({ views: `${Deno.cwd()}/templates/` });
+
+const validator = z.object({
+    name: z.string().min(4, {message : "The course name should be a string of at least 4 characters."}),
+  });
 
 const showCourses = async (c) => {
     return c.html(
@@ -18,6 +23,18 @@ const showCourse = async (c) => {
 
 const createCourse = async (c) => {
     const body = await c.req.parseBody();
+    const validationResult = validator.safeParse(body);
+
+    if (!validationResult.success) {
+        return c.html(
+            eta.render("courses.eta", {
+                ...body,
+                errors: validationResult.error.format(),
+                courses: await courseService.listCourses()
+            })
+        );
+    }
+
     await courseService.createCourse(body);
     return c.redirect("/courses");
 };
